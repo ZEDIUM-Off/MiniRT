081037fe5566e3925f1fd158aefaba4bedd849b9
Matisse Chenavas <mchenava@z3r3p5.42lyon.fr>
2024-03-04 17:03:14 +0100

    WIP Shpere

--- RayTracer/minirt.c
+++ RayTracer/minirt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: mchenava <mchenava@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 11:03:15 by mchenava          #+#    #+#             */
-/*   Updated: 2024/03/04 14:30:00 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2024/03/04 17:02:40 by mchenava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,25 @@ int	main(int argc, char **argv)
 {
 	t_rt	rt;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <scene_description_file.rt>\n", argv[0]);
-		return (EXIT_FAILURE);
-	}
+	(void)argc;
+	(void)argv;
+	// if (argc != 2)
+	// {
+	// 	fprintf(stderr, "Usage: %s <scene_description_file.rt>\n", argv[0]);
+	// 	return (EXIT_FAILURE);
+	// }
 	if (init_rt(&rt) != CONTINUE)
 		return (1);
-	if (!parse_read_file(argv[1], &rt))
-	{
-		printf("Error\nFailed to parse the scene file: %s\n%s\n", argv[1],
-			rt.err_msg);
-		clean_rt(&rt);
-		return (EXIT_FAILURE);
-	}
+	// if (!parse_read_file(argv[1], &rt))
+	// {
+	// 	printf("Error\nFailed to parse the scene file: %s\n%s\n", argv[1],
+	// 		rt.err_msg);
+	// 	clean_rt(&rt);
+	// 	return (EXIT_FAILURE);
+	// }
 	printf("Parsing done...");
 	printf("lauching loop...");
-	// mlx_loop(rt.mxv.mlx);
+	mlx_loop(rt.mxv.mlx);
 	quit_rt(&rt);
 	printf("exiting...");
 }
