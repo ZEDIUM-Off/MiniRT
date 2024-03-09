55f44f3193c82d597778ee8f1897cbc0d07a8dce
agaley <agaley@student.42lyon.fr>
2024-03-09 01:48:40 +0100

    [GL] Fix projection

--- RayTracer/includes/const.h
+++ RayTracer/includes/const.h
@@ -6,15 +6,15 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 13:21:00 by mchenava          #+#    #+#             */
-/*   Updated: 2024/02/20 15:43:40 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2024/03/09 01:47:49 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONST_H
 # define CONST_H
 
-# define WIDTH 800
-# define HEIGHT 800
+# define WIDTH 1280
+# define HEIGHT 720
 
 # define RED_MASK 0x00FF0000
 # define GREEN_MASK 0x0000FF00
--- RayTracer/rendering/projection.c
+++ RayTracer/rendering/projection.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   projection.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mchenava <mchenava@student.42.fr>          +#+  +:+       +#+        */
+/*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 11:00:11 by mchenava          #+#    #+#             */
-/*   Updated: 2024/03/07 16:18:03 by mchenava         ###   ########.fr       */
+/*   Updated: 2024/03/09 01:22:38 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	cam_proj(t_rt *rt, t_uniforms *u)
 	t_cam	*cam;
 
 	cam = (t_cam *)&rt->cam;
-	make_perspective_matrix(u->proj, radians(cam->fov), WIDTH / HEIGHT,
-		(float[2]){0.1, 1000.0});
+	make_perspective_matrix(u->proj, radians(cam->fov), (float)WIDTH
+		/ (float)HEIGHT, (float[2]){0.1, 1000.0});
 	look_at(u->view, cam->pos, cam->target, (t_vec3){0, 1, 0});
 }
