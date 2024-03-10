45a10aa0d9daf95cecb39176d99140a5090b81d9
agaley <agaley@student.42lyon.fr>
2024-03-10 22:42:59 +0100

    [RT] Fix cylinder intersection

--- RayTracer/shader/intersect_cylinders.c
+++ RayTracer/shader/intersect_cylinders.c
@@ -6,25 +6,63 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 04:52:23 by agaley            #+#    #+#             */
-/*   Updated: 2024/03/07 17:19:11 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2024/03/10 22:31:44 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+static bool	check_intersection_conditions(float t, t_ray *ray, t_shape *cy,
+		t_hit *hit, float *closest_t)
+{
+	t_vec3				hit_point;
+	t_vec3				cy_to_hit_point;
+	float				hit_to_axis_projection;
+	t_cylinder_props	*props;
+
+	props = cy->properties;
+	hit_point = add_vec3s(ray->ori, mult_vec3_scalar(ray->dir, t));
+	cy_to_hit_point = sub_vec3s(hit_point, cy->position);
+	hit_to_axis_projection = dot_vec3s(cy_to_hit_point, props->axis);
+	if (fabs(hit_to_axis_projection) <= props->height / 2)
+	{
+		if (t < *closest_t)
+		{
+			*closest_t = t;
+			hit->distance = t;
+			hit->point = hit_point;
+			hit->normal = norm_vec3(sub_vec3s(cy_to_hit_point,
+						mult_vec3_scalar(props->axis, hit_to_axis_projection)));
+			hit->color = cy->color;
+			return (true);
+		}
+	}
+	return (false);
+}
+
+static bool	find_intersections(t_ray *ray, t_shape *cy, t_hit *hit, t_vec2 t)
+{
+	float	closest_t;
+	bool	found_intersection;
+
+	closest_t = INFINITY;
+	found_intersection = false;
+	if (t.x >= EPSILON && check_intersection_conditions(t.x, ray, cy, hit,
+			&closest_t))
+		found_intersection = true;
+	if (t.y >= EPSILON && check_intersection_conditions(t.y, ray, cy, hit,
+			&closest_t))
+		found_intersection = true;
+	return (found_intersection);
+}
 
 bool	intersect_cylinder(t_ray *ray, t_shape *cy, t_hit *hit)
 {
 	t_vec3				oc;
 	t_vec3				coeff;
 	float				discriminant;
-	float				t1;
-	float				t2;
-	float				t;
-	t_vec3				hit_point;
-	t_vec3				cy_to_hit_point;
-	float				hit_to_axis_projection;
+	t_vec2				t;
 	t_cylinder_props	*props;
-	int					i;
 
 	props = cy->properties;
 	oc = sub_vec3s(ray->ori, cy->position);
@@ -37,30 +75,7 @@ bool	intersect_cylinder(t_ray *ray, t_shape *cy, t_hit *hit)
 	discriminant = coeff.y * coeff.y - 4 * coeff.x * coeff.z;
 	if (discriminant < 0)
 		return (false);
-	t1 = (sqrt(discriminant) - coeff.y) / (2 * coeff.x);
-	t2 = (sqrt(discriminant) - coeff.y) / (2 * coeff.x);
-	i = 0;
-	while (i < 2)
-	{
-		if (i == 0 && t1 > EPSILON)
-			t = t1;
-		else if (i == 1 && t2 > EPSILON)
-			t = t2;
-		else
-			return (false);
-		hit_point = add_vec3s(ray->ori, mult_vec3_scalar(ray->dir, t));
-		cy_to_hit_point = sub_vec3s(hit_point, cy->position);
-		hit_to_axis_projection = dot_vec3s(cy_to_hit_point, props->axis);
-		if (fabs(hit_to_axis_projection) <= props->height / 2)
-		{
-			hit->distance = t;
-			hit->point = hit_point;
-			hit->normal = norm_vec3(sub_vec3s(cy_to_hit_point,
-						mult_vec3_scalar(props->axis, hit_to_axis_projection)));
-			hit->color = cy->color;
-			return (true);
-		}
-		i++;
-	}
-	return (false);
+	t.x = (sqrt(discriminant) - coeff.y) / (2 * coeff.x);
+	t.y = -(sqrt(discriminant) + coeff.y) / (2 * coeff.x);
+	return (find_intersections(ray, cy, hit, t));
 }
