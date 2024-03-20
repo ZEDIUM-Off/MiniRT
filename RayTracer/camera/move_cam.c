24164bdc9e9b3598d6351b2853daeb00441d3ec5
Matisse Chenavas <mchenava@z3r2p5.42lyon.fr>
2024-03-20 12:52:47 +0100

    normed all ready to submit v1

--- RayTracer/camera/move_cam.c
+++ RayTracer/camera/move_cam.c
@@ -6,7 +6,7 @@
 /*   By: mchenava <mchenava@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:42:38 by  mchenava         #+#    #+#             */
-/*   Updated: 2024/03/20 11:50:37 by mchenava         ###   ########.fr       */
+/*   Updated: 2024/03/20 12:52:27 by mchenava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ void	cam_rotate(t_rt *rt, float delta_x, float delta_y)
 	t_vec3	pos2origin;
 
 	cam = (t_cam *)&rt->cam;
-	yaw = cam->speed * delta_x / 2;
-	pitch = cam->speed * delta_y / 2;
+	yaw = cam->speed * delta_x / 5;
+	pitch = cam->speed * delta_y / 5;
 	cam2target = sub_vec3s(cam->target, cam->pos);
 	cam->dir = norm_vec3(cam2target);
 	cam->dir = vec3_rotate(cam->dir, pitch, cam->right);
@@ -42,8 +42,8 @@ void	cam_translate(t_rt *rt, int x_offset, int y_offset)
 
 	cam = &rt->cam;
 	translation = add_vec3s(scale_vec3s(norm_vec3(cam->right), -x_offset
-				* cam->speed), scale_vec3s(norm_vec3(cam->up), y_offset
-				* cam->speed));
+				* cam->speed * 2), scale_vec3s(norm_vec3(cam->up), y_offset
+				* cam->speed * 2));
 	cam->pos = add_vec3s(cam->pos, translation);
 	cam->target = add_vec3s(cam->target, translation);
 }
@@ -54,7 +54,7 @@ void	cam_zoom(t_rt *rt, int zoom)
 	t_vec3	translation;
 
 	cam = &rt->cam;
-	translation = scale_vec3s(cam->dir, zoom);
+	translation = scale_vec3s(cam->dir, zoom * 2);
 	cam->pos = add_vec3s(cam->pos, translation);
 	cam->target = add_vec3s(cam->target, translation);
 }
