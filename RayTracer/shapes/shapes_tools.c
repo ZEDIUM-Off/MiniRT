f19f7f99432125189efefacef5e88d90f4b2e736
agaley <agaley@student.42lyon.fr>
2024-03-20 01:31:52 +0100

    Fix norm

--- RayTracer/shapes/shapes_tools.c
+++ RayTracer/shapes/shapes_tools.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   shapes_tools.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mchenava <mchenava@student.42.fr>          +#+  +:+       +#+        */
+/*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 15:25:02 by  mchenava         #+#    #+#             */
-/*   Updated: 2024/03/19 18:14:09 by mchenava         ###   ########.fr       */
+/*   Updated: 2024/03/20 01:31:29 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ bool	modify_mesh_details(t_rt *rt)
 {
 	char	*input;
 
-	do
+	write(1, "Enter the mesh detail: ", 24);
+	input = get_stdin();
+	while (!is_float(input) || !is_in_range(input, 8, 64))
 	{
 		write(1, "Enter the mesh detail: ", 24);
 		input = get_stdin();
-	} while (!is_float(input) && !is_in_range(input, 8, 64));
+	}
 	rt->mesh_detail = ft_atof(input);
 	free(input);
 	return (true);
